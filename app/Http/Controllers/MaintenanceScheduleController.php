053466e680ad9021f238d54a946c88918b84e006
Brent <brent2890@gmail.com>
2015-04-25 23:42:14 -0400

    Fix bug on ScheduleController regarding Auth class import

--- app/Http/Controllers/MaintenanceScheduleController.php
+++ app/Http/Controllers/MaintenanceScheduleController.php
@@ -7,6 +7,7 @@ use App\Models\MaintenanceSchedule;
 use App\Models\MaintenanceType;
 use App\Models\Vehicle;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class MaintenanceScheduleController extends Controller {
 
